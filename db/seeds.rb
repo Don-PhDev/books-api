412ae6c87f60357e29423a928661f29f1c94f884
Raymond <raymond@philippinedev.com>
2021-10-25 16:45:12 +0800

    added seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def create_users
+  User.destroy_all
+
+  10.times do
+    users = User.create(
+      email: Faker::Name.unique.first_name + "@gmail.com", 
+      password: "password"
+    )
+  end
+
+  p "Created #{User.count} users"
+end
+
+def create_authors
+  Author.destroy_all
+
+  8.times do
+    authors = Author.create(
+      name: Faker::Book.unique.author,
+      user_id: User.pluck(:id).sample
+    )
+  end
+
+  p "Created #{Author.count} authors"
+end
+
+def create_books
+  Book.destroy_all
+
+  30.times do
+    books = Book.create(
+      title: Faker::Book.unique.title,
+      publisher: Faker::Book.publisher,
+      published_on: Date.today,
+      author_id: Author.pluck(:id).sample,
+      user_id: User.pluck(:id).sample
+    )
+  end
+
+  p "Created #{Book.count} books"
+end
+
+create_users
+create_authors
+create_books
