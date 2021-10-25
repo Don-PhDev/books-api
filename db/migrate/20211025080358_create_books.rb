3b9dd8b283c47ac480d19a5680aee5ee55e5904f
Raymond <raymond@philippinedev.com>
2021-10-25 16:44:34 +0800

    added model associations and validations

--- db/migrate/20211025080358_create_books.rb
+++ db/migrate/20211025080358_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-      t.string :title
-      t.string :publisher
+      t.string :title, null: false, index: { unique: true }
+      t.string :publisher, null: false
       t.string :published_on
       t.references :author, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
