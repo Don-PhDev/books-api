7cb76f12f96982646e5dfaca50d00e01575d7ccc
Raymond <raymond@philippinedev.com>
2021-10-28 14:05:02 +0800

    refactor

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :book do
     title { Faker::Book.unique.title }
-    publisher { Faker::Name.unique.name }
+    publisher { Faker::Book.publisher }
     published_on { Date.today }
     author_id { Author.pluck(:id).sample }
     user_id { User.pluck(:id).sample }
