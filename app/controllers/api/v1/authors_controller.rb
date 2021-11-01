4a8eb3c4e1173cc30e0c69d918eae7f057aa93e2
Raymond <raymond@philippinedev.com>
2021-11-01 20:33:24 +0800

    TDD for author controller action create method index and show

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -1,7 +1,37 @@
 class Api::V1::AuthorsController < ApplicationController
-  def index
-    authors = Author.all
+  before_action :set_author, only: [:show]
 
-    render json: authors
+  def index
+    @authors = Author.all
+
+    render json: @authors,
+      except: [
+        :created_at,
+        :updated_at
+      ]
+  end
+
+  def show
+    render json: @author
+  end
+
+  def create
+    author = Author.new(author_params)
+
+    if author.save
+      render json: @authors, status: :created
+    else
+      render json: author.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name, :user_id)
+  end
+
+  def set_author
+    @author = Author.find(params[:id])
   end
 end
--- spec/requests/api/v1/authors_spec.rb
+++ spec/requests/api/v1/authors_spec.rb
@@ -1,6 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::AuthorsController do
+  let!(:user) { create :user }
+  let(:json_format) do
+    {
+      except: %i[
+        created_at
+        updated_at
+      ]
+    }
+  end
+  let(:name) { Faker::Book.author }
+  let(:params) do
+    {
+      author: {
+        name: name,
+        user_id: User.pluck(:id).sample
+      }
+    }
+  end
+
   describe "GET /index" do
+    let!(:authors) { create_list(:author, 3) }
+    let(:expected_body) { authors.as_json(json_format) }
+
+    subject { get '/api/v1/authors' }
+
+    it 'returns list of all authors' do
+      subject
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)).to eq(expected_body)
+    end
+  end
+
+  describe "GET /show" do
+    let(:author) { create :author }
+
+    subject { get "/api/v1/authors/#{author.id}" }
+
+    it 'returns an author' do
+      subject
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "POST /create" do
+    subject { post '/api/v1/authors', params: params }
+
+    it 'creates new author' do
+      expect{ subject }.to change { Author.count }.by 1
+    end
+
+    it 'returns success status' do
+      subject
+      expect(response.status).to eq(201)
+    end
+
+    context 'with invalid input' do
+      let(:name) { nil }
+
+      it 'return failed status' do
+        subject
+        expect(response.status).to eq(422)
+      end
+    end
   end
 end
